b43e68496f6700052b343aa0bf2d398656e01dbc
Danielle Smalley <smalley.danielle@gmail.com>
2021-06-22 15:20:16 -0500

    Identity Samples complete commit

--- StoreFront/Controllers/HomeController.cs
+++ StoreFront/Controllers/HomeController.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+//***On my project, identity/identitysamples issue that we had to fix (OWIN) :
+//appSetting owin:AutomaticAppStartup with a value of "false" ***
 
 namespace StoreFront.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        [Authorize]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your app description page.";
 
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
